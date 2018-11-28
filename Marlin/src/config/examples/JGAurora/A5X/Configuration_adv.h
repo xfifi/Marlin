b3edcda54a7272c36034064de2dce60d75671d4a
Philippe RODIER <philippe.rodier@laposte.net>
2018-11-28 22:06:48 +0100

    Update Configuration_adv.h

--- Marlin/src/config/examples/JGAurora/A5X/Configuration_adv.h
+++ Marlin/src/config/examples/JGAurora/A5X/Configuration_adv.h
@@ -398,9 +398,9 @@
 // Homing hits each endstop, retracts by these distances, then does a slower bump.
 #define X_HOME_BUMP_MM 5
 #define Y_HOME_BUMP_MM 5
-#define Z_HOME_BUMP_MM 2
+#define Z_HOME_BUMP_MM 3
 #define HOMING_BUMP_DIVISOR { 2, 2, 4 }  // Re-Bump Speed Divisor (Divides the Homing Feedrate)
-//#define QUICK_HOME                     // If homing includes X and Y, do a diagonal move initially
+#define QUICK_HOME                     // If homing includes X and Y, do a diagonal move initially
 
 // When G28 is called, this option will make Y home before X
 //#define HOME_Y_BEFORE_X
